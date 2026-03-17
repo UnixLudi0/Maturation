e1d2bedaf43abb1272ba2df6fca42fd01b21fad6
UnixLudi0 <95374315+UnixLudi0@users.noreply.github.com>
2026-03-17 22:27:36 +0300

    Minor toggle keymode improvement

--- mango/scripts/toggle_keymode.sh
+++ mango/scripts/toggle_keymode.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mode=$(mmsg -b | grep keymode | awk '{print $3}' | head -1)
-echo "$mode" >> /tmp/mango_debug.log
-if [ "$mode" = "default" ]; then
+
+mode=$(mmsg -b | awk '{print $NF}')
+
+if [ "$mode" == "default" ]; then
     mmsg -d setkeymode,special
 else
     mmsg -d setkeymode,default
