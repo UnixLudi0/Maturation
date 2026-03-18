7aae60d121176e8c0c38d1542b9398b56e733328
flyofk <ggnekitos@gmail.com>
2026-03-18 09:27:55 +0300

    added keymodes notifications

--- mango/scripts/toggle_keymode.sh
+++ mango/scripts/toggle_keymode.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+enable_notifications=true
 mode=$(mmsg -b | awk '{print $NF}')
 
-if [ "$mode" == "default" ]; then
+if [[ "$mode" = "default" && $enable_notifications = true ]]; then
     mmsg -d setkeymode,special
-else
+    notify-send "Keymode: Special"
+elif [[ "$mode" = "special" && $enable_notifications = true ]]; then
+    mmsg -d setkeymode,default
+    notify-send "Keymode: Default"
+elif [[ "$mode" = "default" && $enable_notifications = false ]]; then
+    mmsg -d setkeymode,special
+elif [[ "$mode" = "special" && $enable_notifications = false ]]; then
     mmsg -d setkeymode,default
 fi
