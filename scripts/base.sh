8c705e38413a27001d7904c2775e661539ba8f67
UnixLudi0 <95374315+UnixLudi0@users.noreply.github.com>
2026-06-05 19:05:39 +0300

    Install yay dependies first. Remove confirmation when install yay

--- scripts/base.sh
+++ scripts/base.sh
@@ -20,12 +20,13 @@ sudo pacman -Sy --noconfirm reflector
 sudo reflector --verbose --country "$(curl -sSL 'https://ifconfig.co/country-iso')" -l 25 --sort age --save /etc/pacman.d/mirrorlist
 sudo sed -i 's/\[options\]/\[options\]\nDisableDownloadTimeout/g' /etc/pacman.conf
 
+sudo pacman -S --noconfirm autoconf automake binutils bison debugedit fakeroot file findutils flex gawk gcc gettext grep groff gzip libtool m4 make pacman patch pkgconf sed texinfo which
+
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -sric
+makepkg -sric --noconfirm
 cd .. && rm -r yay
 
-yay -S --noconfirm autoconf automake binutils bison debugedit fakeroot file findutils flex gawk gcc gettext grep groff gzip libtool m4 make pacman patch pkgconf sed texinfo which
 yay -S --noconfirm linux-cachyos linux-cachyos-headers linux-zen linux-zen-headers
 yay -S --noconfirm git cmake mkinitcpio-firmware
 yay -S --noconfirm chwd
