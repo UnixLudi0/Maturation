03c208828deaa612903547108e28fe265316542f
flyofk <ggnekitos@gmail.com>
2026-06-23 16:52:32 +0300

    update keymode notification script

--- mango/scripts/toggle_keymode.sh
+++ mango/scripts/toggle_keymode.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
 enable_notifications=true
-mode=$(mmsg -b | awk '{print $NF}')
 
-if [[ "$mode" = "default" && $enable_notifications = true ]]; then
-    mmsg -d setkeymode,special
-    notify-send "Keymode: Special"
-elif [[ "$mode" = "special" && $enable_notifications = true ]]; then
-    mmsg -d setkeymode,default
-    notify-send "Keymode: Default"
-elif [[ "$mode" = "default" && $enable_notifications = false ]]; then
-    mmsg -d setkeymode,special
-elif [[ "$mode" = "special" && $enable_notifications = false ]]; then
-    mmsg -d setkeymode,default
+mode=$(mmsg get keymode | grep -o '"keymode":"[^"]*"' | cut -d'"' -f4)
+
+if [[ "$mode" = "default" ]]; then
+    mmsg dispatch setkeymode,special
+    [[ "$enable_notifications" = true ]] && notify-send "Keymode: Special"
+elif [[ "$mode" = "special" ]]; then
+    mmsg dispatch setkeymode,default
+    [[ "$enable_notifications" = true ]] && notify-send "Keymode: Default"
 fi
